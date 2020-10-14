a84ad93414c93b0131d26ae19236e6c69a525a36
jonhermsen-UNO <jonhermsen@unomaha.edu>
2020-10-13 19:46:00 -0500

    feat: add DB models and migration

--- app/models.py
+++ app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Species(models.Model):
+    species_name = models.CharField(max_length=50)
+    weight_uom = models.CharField(max_length=3)
+
+    def __str__(self):
+        return self.species_name
+
+
+class Animal(models.Model):
+    species = models.ForeignKey(Species, on_delete=models.CASCADE)
+    name = models.CharField(max_length=150)
+    birth_date = models.DateTimeField()
+    weight = models.DecimalField(max_digits=5, decimal_places=2)
+    adoption_fee = models.DecimalField(max_digits=5, decimal_places=2)
+    is_male = models.BooleanField()
+
+    def __str__(self):
+        return "%s the %s" % (name, species)
