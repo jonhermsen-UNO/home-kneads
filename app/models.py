e6baac139eb21b0108acb7020b3c5cd15b18b53f
jonhermsen-UNO <jonhermsen@unomaha.edu>
2020-10-20 05:06:32 -0500

    feat: add icon property to Species

--- app/models.py
+++ app/models.py
@@ -5,6 +5,21 @@ class Species(models.Model):
     name = models.CharField(max_length=50)
     weight_uom = models.CharField(max_length=3)
 
+    def get_icon(self):
+        """This method attempts to match a species name with a Font Awesome icon.
+        If a match is found, it is returned, otherwise a default icon is returned.
+        """
+        icon_options = {
+            'bird': 'dove',
+            'cat': 'cat',
+            'dog': 'dog',
+            'fish': 'fish',
+            'frog': 'frog',
+            'horse': 'horse',
+        }
+        return icon_options.get(self.name.lower(), 'paw')
+    icon = property(get_icon)
+
     def __str__(self):
         return "%s (%s)" % (self.name, self.weight_uom)
 
